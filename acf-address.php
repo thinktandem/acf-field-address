083a5c32de768912b78c29a01f91d44deb135516
John Ouellet <john@thinktandem.io>
2020-05-18 14:07:56 -0400

    Just use the plugin

--- acf-address.php
+++ acf-address.php
@@ -4,14 +4,18 @@
 Plugin Name: Advanced Custom Fields: Address
 Plugin URI: https://github.com/strickdj/acf-field-address
 Description: An Advanced Custom Field for working with address information.
-Version: 5.1.0
+Version: 5.0.6
 Author: Daris Strickland
 Author URI: http://daris-strickland.com/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-define('ACF_ADDRESS_PLUGIN_PATH', plugin_dir_path(__FILE__) . 'src/app/plugins/acf-address/');
-define('ACF_ADDRESS_PLUGIN_URL', plugins_url('', __FILE__) . '/src/app/plugins/acf-address');
+define('ACF_ADDRESS_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('ACF_ADDRESS_PLUGIN_URL', plugins_url('', __FILE__));
 
-require "src/app/plugins/acf-address/bootstrap.php";
+//var_dump(ACF_ADDRESS_PLUGIN_PATH);
+//var_dump(ACF_ADDRESS_PLUGIN_URL);
+//die();
+
+require "bootstrap.php";
